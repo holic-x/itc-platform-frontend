58af2eb20b4133d88376d807c1ceca95e55035a2
holic-x <holic-x@foxmail.com>
2024-04-29 20:29:00 +0800

    init ： BI 图表智能分析页面初始化
    BI 图表智能分析页面初始化

--- src/pages/User/Bi/Chart/index.tsx
+++ src/pages/User/Bi/Chart/index.tsx
@@ -1,12 +1,28 @@
-
 import React from 'react';
-import { PageContainer } from '@ant-design/pro-components';
+import {PageContainer} from '@ant-design/pro-components';
+import { Col, Card, Row } from 'antd';
 
 const Index: React.FC = () => {
-
   return (
-
-    <PageContainer>初始化页面配置</PageContainer>
+    <PageContainer>
+      <Row
+        gutter={24}
+        style={{
+          marginTop: 24,
+        }}
+      >
+        <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+          <Card>
+            提交表单
+          </Card>
+        </Col>
+        <Col xl={12} lg={24} md={24} sm={24} xs={24}>
+          <Card>
+            图表
+          </Card>
+        </Col>
+      </Row>
+    </PageContainer>
 
   );
 };
